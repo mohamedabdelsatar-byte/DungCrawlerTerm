76588b99dd9ea2abf003e00ae42386c31b1b698f
mohamedabdelsatar-byte <183103008+mohamedabdelsatar-byte@users.noreply.github.com>
2026-04-05 22:08:08 +0200

    Tested sprites, movement, changed a bit with the CMakeList.

--- src/SFML_test.cpp
+++ src/SFML_test.cpp
@@ -1,9 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+/*void centerOrigin(sf::Shape& shape){
+    auto bound = shape.getGlobalBounds();
+    shape.setOrigin({bound.position.x + bound.size.x/2.f, bound.position.y + bound.size.y/2.f});
+}*/
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "DungCrawler");
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "DungCrawler");
+
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("Assets/Textures/Witch.png");
+    sf::Sprite player(playerTexture);
+    player.setScale(sf::Vector2f(10.f, 10.f));
+    player.setPosition({960.f, 540.f});
+
 
     //MAIN LOOP
     while (window.isOpen()) {
@@ -13,29 +25,42 @@ int main() {
                 window.close();
             }
         }
+
+        //Player Movement
+        sf::Vector2f vPlayerMovement(0.0f, 0.0f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            vPlayerMovement += sf::Vector2f(0.0f, -1.0f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            vPlayerMovement += sf::Vector2f(-1.0f, 0.0f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            vPlayerMovement += sf::Vector2f(0.0f, 1.0f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            vPlayerMovement += sf::Vector2f(1.0f, 0.0f);
+        }
+
+        player.move(vPlayerMovement);
+
         window.clear(sf::Color::Black);
         //DRAW
-        //rectangle
-        sf::RectangleShape rectangle({120.f, 50.f});
-        rectangle.setPosition({100.f, 50.f});
-        rectangle.setFillColor(sf::Color::Blue);
-        rectangle.setSize({100.f, 100.f});
 
-        window.draw(rectangle);
+        sf::RectangleShape Door1({200.f, 200.f});
+        Door1.setOrigin(sf::Vector2f(8, 8));
+        Door1.setPosition({960.f, 540.f});
 
-        sf::CircleShape circle(50.f);
-        circle.setPosition({50.f, 100});
-        circle.setFillColor(sf::Color::Green);
-        circle.setRadius(50.f);
 
-        window.draw(circle);
+        Door1.setFillColor(sf::Color::Blue);
 
-        sf::CircleShape triangle(70.f, 3);
-        triangle.setPosition({120.f, 90.f});
-        triangle.setFillColor(sf::Color::Yellow);
 
-        window.draw(triangle);
 
+       // window.draw(Door1);
+        window.draw(player);
 
 
         window.display();
@@ -49,3 +74,43 @@ int main() {
 return 0;
 }
 
+
+
+
+/* DOORS
+        sf::RectangleShape Door1({50.f, 120.f});
+        centerOrigin(Door1);
+        Door1.setPosition({100.f, 550.f});
+        Door1.setFillColor(sf::Color::Blue);
+
+
+        sf::RectangleShape Door2({50.f, 120.f});
+        centerOrigin(Door2);
+        Door2.setPosition({200.f, 550.f});
+        Door2.setFillColor(sf::Color::Green);
+
+
+        sf::RectangleShape Door3({50.f, 120.f});
+        centerOrigin(Door3);
+        Door3.setPosition({300.f, 550.f});
+        Door3.setFillColor(sf::Color::Red);
+
+
+        sf::RectangleShape Door4({50.f, 120.f});
+        centerOrigin(Door4);
+        Door4.setPosition({400.f, 550.f});
+        Door4.setFillColor(sf::Color::Yellow);
+
+        sf::RectangleShape Door5({50.f, 120.f});
+        centerOrigin(Door5);
+        Door5.setPosition({500.f, 550.f});
+        Door5.setFillColor(sf::Color::White);
+
+
+
+        window.draw(Door1);
+        window.draw(Door2);
+        window.draw(Door3);
+        window.draw(Door4);
+        window.draw(Door5);
+        */
